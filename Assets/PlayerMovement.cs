9c96cda0d6beb600e78c25c52bd7317c348ac9c1
FairoLegends <faiqfebrianpro@gmail.com>
2025-02-20 23:38:56 +0700

    Project Files

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Mengatur kecepatan gerak player
+    public float speed;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Horizontal = A atau Kiri (-) dan D atau Kanan (+)
+        float horizontal = Input.GetAxis("Horizontal");
+        //Horizontal = W atau Kedepan (+) dan S atau Kebelakang ()
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
+
+        rb.velocity = movementDirection * speed * Time.fixedDeltaTime;
+    }
+}
